57be19ec4dbdf7c9dda93288ae56a53c8673a363
Zippo Xie <zippo@zippos-macbook-pro.local>
2018-09-19 11:39:16 -0700

    Initial Commit

---
+++ V2ex_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  V2ex_iOS
+//
+//  Created by zippo on 9/19/18.
+//  Copyright © 2018 zippo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ V2ex_iOSTests/V2ex_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  V2ex_iOSTests.swift
+//  V2ex_iOSTests
+//
+//  Created by zippo on 9/19/18.
+//  Copyright © 2018 zippo. All rights reserved.
+//
+
+import XCTest
+@testable import V2ex_iOS
+
+class V2ex_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
